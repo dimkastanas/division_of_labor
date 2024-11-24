05e199f7ab58323a3cd4359e4b5d8b10e5dba91a
Robert Hawkins <hawkrobe@gmail.com>
2024-11-24 07:11:20 -0800

    fix model to work with json

--- modeling/run_AIS.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash#
-# parallel --bar --colsep ',' "sh ./run_AIS.sh {1} {2}" :::: input/BF_grid.csv 
-webppl BDA.wppl --require ./refModule/ --require webppl-csv -- --chainNum $1 --model $2 --AIS true
